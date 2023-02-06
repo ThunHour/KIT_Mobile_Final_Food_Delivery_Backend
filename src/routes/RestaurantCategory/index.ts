a74fe82b9253b7dd6a1c219749693237b17bf7b6
SeakKimhour <seakkimhor20@kit.edu.kh>
2023-02-06 21:22:55 +0700

    remove folder

--- src/routes/RestaurantCategory/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import res from "../RestaurantCategory/restaurantCatrgory.controller";
-const uploadFile = require("multer")();
-export default () => {
-  const router = Router();
-  router.post("/create", res.createPartner);
-
-  return router;
-};
